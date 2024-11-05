47b1d636d05d087806a792c0bb9eee4ec4fcf04c
Bugra020 <bugrakaya020@gmail.com>
2024-11-05 15:47:28 +0300

    asyncio implmentation

--- peer.py
+++ peer.py
@@ -1,28 +1,39 @@
-import socket
+import asyncio
+
+class PeerCollection:
+    def __init__(self):
+        self.peers = []
+
+    def add_peer(self, peer):
+        self.peers.append(peer)
+
+    def __iter__(self):
+        return iter(self.peers)
 
 class Peer(object):
-
-    def __init__(self, ip, port, debug_mode):
+    def __init__(self, ip, port):
+        self.has_handshaked = False
+        self.healthy = False
         self.ip = ip
-        self.port  = port
-        self.debug_mode = debug_mode
+        self.port = port
+        self.writer = ''
 
     def _debug(self, msg):
-        if self.debug_mode:
-            print(msg)
+        print(msg)
 
-    def connect(self):
-        try:
-            self.socket = socket.create_connection((self.ip, self.port), timeout=2)
-            self.socket.setblocking(False)
+    async def connect(self):
+        try:        
+            self.reader, self.writer = await asyncio.open_connection(self.ip, self.port)
             self._debug("Connected to peer ip: {} - port: {}".format(self.ip, self.port))
             self.healthy = True
 
         except Exception as e:
             self._debug("Failed to connect to peer (ip: %s - port: %s - %s)" % (self.ip, self.port, e.__str__()))
-            return False
 
-        return True
+    async def close(self):
+        if self.writer:
+            self.writer.close()
+            await self.writer.wait_closed()
 
     def send_handshake(self):
         pass
--- peer_manager.py
+++ peer_manager.py
@@ -1,21 +1,24 @@
 import peer
+import asyncio
 
 class PeerManager():
     def __init__(self, debug_mode):
-        self.peers = []
         self.debug_mode = debug_mode
+        self.peers = peer.PeerCollection()
     
     def _debug(self, msg):
         if self.debug_mode:
             print(msg)
     
-    def connect_peers(self, peer_list):
-        self._create_peers(peer_list)
-        
+    async def connect_peers(self, peer_list):
+        self.create_peers(peer_list)
+        tasks = []
         for peer in self.peers:
-            peer.connect()
-
-    def _create_peers(self, peer_list_data):
-        for peer_data in peer_list_data:
-            new_peer = peer.Peer(peer_data[0], peer_data[1], True)
-            self.peers += new_peer 
+            tasks.append(asyncio.create_task(peer.connect()))
+        await asyncio.gather(*tasks, return_exceptions=False)
+        for peer in self.peers:
+            await peer.close()
+    
+    def create_peers(self, peer_list):
+        for peer_data in peer_list:
+            self.peers.add_peer(peer.Peer(ip=peer_data[0], port=peer_data[1]))
